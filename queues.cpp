ada27cada48edc452f9cca9b21ec8be78561a1b1
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-06-12 10:05:02 +0700

    create void insert on public queues class

--- queues.cpp
+++ queues.cpp
@@ -2,11 +2,21 @@
 using namespace std;
 
 class Queues{
-    int frot, rear, max = 5;
+    int front, rear, max = 5;
     int queue_array[5];
 
     public:
         Queues(){
+            front = -1;
+            rear = -1;
+        }
+
+        void insert(){
+            int num;
+            cout << "Enter Number: ";
+            cin >> num;
+            cout << endl;
+
             
         }
 };
